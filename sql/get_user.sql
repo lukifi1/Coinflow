adf5cc2af60e325fd8920ded4e1c7dac9e368f0d
AustrianToast <austriantoast@hopeless-cloud.xyz>
2025-04-10 17:35:44 +0200

    add get_user function

---
+++ sql/get_user.sql
@@ -0,0 +1,14 @@
+-- DROP FUNCTION get_user(uuid);
+CREATE OR REPLACE FUNCTION public.get_user(in_uuid uuid) RETURNS TABLE (username text, email text, password_hash text)
+	LANGUAGE plpgsql STRICT
+	AS $$
+BEGIN
+	RETURN QUERY SELECT u.username, u.email, u.password_hash
+				  FROM users u
+                  WHERE  u.uuid = in_uuid;
+
+    IF NOT FOUND THEN
+        RAISE EXCEPTION 'No user found for uuid: %', in_uuid;
+    END IF;
+END;
+$$;
