e6d31491684b9d7289e7537cdf3b9ffd418cf51b
AustrianToast <austriantoast@hopeless-cloud.xyz>
2025-04-10 22:53:13 +0200

    make use of promise methods

--- app.js
+++ app.js
@@ -38,12 +38,13 @@ app.get("/api", (req, res) => {
 
 // Healthcheck for docker and for whoever wants it
 app.get("/api/healthcheck", async (req, res) => {
-    const result = await pool.query("SELECT $1::text as name", ["Works"])
-    if (result.rows[0].name = "Works") {
-        res.status(200).end()
-    } else {
-        res.status(400).end()
-    }
+    pool.query("SELECT $1::text as name", ["Works"])
+        .then((result) => {
+            res.send(result)
+        })
+        .catch((error) => {
+            res.status(500).json({ error })
+        })
 })
 
 // Fetch user info from db as json
@@ -52,12 +53,18 @@ app.get("/api/user/:uuid", async (req, res) => {
     // Where I got the expression from https://stackoverflow.com/a/38191104
     // This Expression only works for UUIDv4, which is the version prostgresql uses
     const regex = new RegExp(/^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/, "i")
-    if (regex.test(req.params.uuid)) {
-        const result = await pool.query("SELECT * FROM get_user($1::uuid)", [req.params.uuid])
-        res.send(result.rows[0])
-    } else {
+    if (!regex.test(req.params.uuid)) {
         res.status(400).end()
+        return
     }
+
+    pool.query("SELECT * FROM get_user($1::uuid)", [req.params.uuid])
+        .then((result) => {
+            res.send(result.rows[0])
+        })
+        .catch((error) => {
+            res.status(500).json({ error })
+        })
 })
 
 app.listen(8080)
