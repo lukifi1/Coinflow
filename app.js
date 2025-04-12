19e7db98572a8c9c0a89ed2ae826cbebad9b8aff
AustrianToast <austriantoast@hopeless-cloud.xyz>
2025-04-13 00:06:11 +0200

    complete CRUD for user stuff

--- app.js
+++ app.js
@@ -18,9 +18,14 @@ const app = express()
 app.use(express.static("www"))
 app.use(express.json())
 
-// Landing/Main Page
-// Takes in no arguments
-// Returns an HTML file or error as json
+function isValidUuid(uuid) {
+    // Helpful documentation https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_expressions
+    // Where I got the expression from https://stackoverflow.com/a/38191104
+    // This Expression only works for UUIDv4, which is the version prostgresql uses
+    const regex = new RegExp(/^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/, "i")
+    return regex.test(uuid)
+}
+
 // TODO: Check if user is logged in and either display the landing or main page
 app.get("/", (req, res) => {
     const filePath = path.join(process.cwd(), "www/index.html")
@@ -60,13 +65,29 @@ app.get("/api/user", (req, res) => {
         })
 })
 
+app.post("/api/user/new", (req, res) => {
+    if (!req.body) {
+        res.status(400).json({ message: "bruh, I need me some json" })
+        return
+    }
+
+    if (!req.body.username || !req.body.email || !req.body.password_hash) {
+        res.status(400).json({ message: "bruh, this shit wrong" })
+        return
+    }
+
+    pool.query("SELECT * FROM insert_user($1, $2, $3)", [req.body.username, req.body.email, req.body.password_hash])
+        .then((result) => {
+            res.json({ uuid: result.rows[0].insert_user })
+        })
+        .catch((error) => {
+            res.status(500).json({ error })
+        })
+})
+
 app.get("/api/user/:uuid", (req, res) => {
-    // Helpful documentation https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_expressions
-    // Where I got the expression from https://stackoverflow.com/a/38191104
-    // This Expression only works for UUIDv4, which is the version prostgresql uses
-    const regex = new RegExp(/^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/, "i")
-    if (!regex.test(req.params.uuid)) {
-        res.status(400).json({ })
+    if (!isValidUuid(req.params.uuid)) {
+        res.status(400).json({ message: "bruh, this shit ain't a valid uuid" })
         return
     }
 
@@ -79,24 +100,44 @@ app.get("/api/user/:uuid", (req, res) => {
         })
 })
 
-app.post("/api/user/new", (req, res) => {
+app.put("/api/user/:uuid", (req, res) => {
+    if (!isValidUuid(req.params.uuid)) {
+        res.status(400).json({ message: "bruh, this shit ain't a valid uuid" })
+        return
+    }
+
     if (!req.body) {
         res.status(400).json({ message: "bruh, I need me some json" })
         return
     }
-    
+
     if (!req.body.username || !req.body.email || !req.body.password_hash) {
         res.status(400).json({ message: "bruh, this shit wrong" })
         return
     }
-    
-    pool.query("SELECT * FROM insert_user($1, $2, $3)", [req.body.username, req.body.email, req.body.password_hash])
-        .then((result) => {
-            res.json({ uuid: result.rows[0].insert_user })
-        })
-        .catch((error) => {
-            res.status(500).json({ error })
-        })
+
+    pool.query("SELECT * FROM update_user($1, $2, $3, $4)", [req.params.uuid, req.body.username, req.body.email, req.body.password_hash])
+    .then((result) => {
+        res.send(result.rows[0])
+    })
+    .catch((error) => {
+        res.status(500).json({ error })
+    })
+})
+
+app.delete("/api/user/:uuid", (req, res) => {
+    if (!isValidUuid(req.params.uuid)) {
+        res.status(400).json({ message: "bruh, this shit ain't a valid uuid" })
+        return
+    }
+
+    pool.query("SELECT * FROM delete_user($1)", [req.params.uuid])
+    .then((result) => {
+        res.send(result.rows[0])
+    })
+    .catch((error) => {
+        res.status(500).json({ error })
+    })
 })
 
 app.listen(8080)
