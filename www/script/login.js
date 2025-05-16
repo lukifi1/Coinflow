3659165175b7b7650e18c08813df8eb25911f1f4
if24b245 <if24b245@technikum-wien.at>
2025-05-16 14:35:18 +0200

    Fixed login and redirect to dashboard

--- www/script/login.js
+++ www/script/login.js
@@ -1,5 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("loginForm");
+    // Same hash as in register.js
+    async function hashPassword(password) {
+        const msgUint8 = new TextEncoder().encode(password);
+        const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);
+        const hashArray = Array.from(new Uint8Array(hashBuffer));
+        const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+        return hashHex;
+    }
+
 
     form.addEventListener("submit", async (e) => {
         e.preventDefault();
@@ -8,11 +17,14 @@ document.addEventListener("DOMContentLoaded", () => {
         const password = form.password.value;
         const rememberMe = form.remember.checked;
 
+// Implement real hashing later (server-side or client-side?)
+        const password_hash = await hashPassword(password);
+
         try {
-            const response = await fetch("/auth/login", {
+            const response = await fetch("/api/user/login", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ email, password })
+                body: JSON.stringify({ email, password_hash }),
             });
 
             const data = await response.json();
@@ -22,8 +34,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 
 
                 storage.setItem("session_token", data.token);
-                storage.setItem("user_uuid", data.user.uuid);
-                storage.setItem("username", data.user.username);
+                storage.setItem("user_uuid", data.uuid);
 
                 window.location.href = "/dashboard.html";
             } else {
